6aebb52ad81d25ab7e2132b38a5a465d3c518b0e
Wolfgangamajeush <jeushceasar.guerrero@gmail.com>
2021-05-26 11:28:15 +0800

    Modified main.c

--- main.c
+++ main.c
@@ -6,3 +6,12 @@ void main()
     printf("Goodbye World!");
     printf("Where in the World?")
 }
+
+#include<stdio.h>
+
+void main()
+{
+    printf("Hello World!");
+    printf("Goodbye World!");
+    printf("Where in the World?")
+}
